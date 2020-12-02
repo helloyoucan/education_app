a6791565922087f6375771ec98e8d089f8714f66
wucanlong <wucanlong@bell.ai>
2020-12-02 12:53:00 +0800

    update

--- lib/pages/practise_page.dart
+++ lib/pages/practise_page.dart
@@ -2,6 +2,7 @@ import 'package:education_app/dao/practise_dao.dart';
 import 'package:education_app/model/practise_model.dart';
 import 'package:education_app/util/adapt_util.dart';
 import 'package:education_app/widget/course_tag_list.dart';
+import 'package:education_app/widget/page_end.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -54,10 +55,13 @@ class _PractisePageState extends State<PractisePage>
           ],
         ),
       ),
-      body: TabBarView(controller: _tabController, children: <Widget>[
-        _practiseView(context),
-        Text('选项卡2'),
-      ]),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          _practiseView(context),
+          _wrongQuestionView,
+        ],
+      ),
     );
   }
 
@@ -114,6 +118,7 @@ class _PractisePageState extends State<PractisePage>
             ],
           ),
         ),
+        PageEnd(),
       ],
     );
   }
@@ -198,49 +203,213 @@ class _PractisePageState extends State<PractisePage>
         : Column(
             children: _practiseModel.list.map((item) {
               return Container(
+                width: double.infinity,
+                margin: EdgeInsets.only(top: Adapt.px(10)),
+                padding: EdgeInsets.fromLTRB(
+                  Adapt.px(10),
+                  Adapt.px(15),
+                  Adapt.px(10),
+                  Adapt.px(15),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(Adapt.px(6))),
+                  border: Border.all(width: 1, color: Color(0xFFEBEBEB)),
+                ),
                 child: Stack(
                   children: [
                     Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(item.section),
-                        Text(item.preview),
+                        Text(
+                          item.section,
+                          style: TextStyle(
+                            fontSize: Adapt.px(14),
+                            color: Color(0xFF333333),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: Adapt.px(10),
+                            bottom: Adapt.px(10),
+                          ),
+                          child: Text(
+                            item.preview,
+                            style: TextStyle(
+                              // height: 2,
+                              fontSize: Adapt.px(14),
+                              color: Color(0xFF010101),
+                            ),
+                          ),
+                        ),
                         item.correctQuestion > 0
                             ? Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   Row(
                                     children: [
-                                      Text(item.correctQuestion.toString()),
-                                      Text('/' +
-                                          item.totalQuestion.toString() +
-                                          '  答题数/总题数'),
+                                      Text(
+                                        item.correctQuestion.toString(),
+                                        style: TextStyle(
+                                          fontSize: Adapt.px(16),
+                                          color: item.correctQuestion ==
+                                                  item.totalQuestion
+                                              ? Color(0xFF03A359)
+                                              : Color(0xFFFE4320),
+                                        ),
+                                      ),
+                                      Text(
+                                        '/' +
+                                            item.totalQuestion.toString() +
+                                            '  答题数/总题数',
+                                        style: TextStyle(
+                                          // height: 2,
+                                          fontSize: Adapt.px(12),
+                                          color: Color(0xFF999999),
+                                        ),
+                                      ),
                                     ],
                                   ),
-                                  item.correctQuestion == item.totalQuestion
-                                      ? Container(
-                                          child: Row(
-                                            children: [
-                                              Icon(Icons.home),
-                                              Text('可真厉害，再接再厉吧^_^')
-                                            ],
+                                  Container(
+                                    height: Adapt.px(25),
+                                    width: Adapt.px(180),
+                                    margin: EdgeInsets.only(top: Adapt.px(4)),
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                      color: Color(0xFFF5F9FC),
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(Adapt.px(13))),
+                                    ),
+                                    child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Icon(
+                                            item.correctQuestion ==
+                                                    item.totalQuestion
+                                                ? Icons.mood
+                                                : Icons.mood_bad,
+                                            color: Color(0xFF999999),
+                                            size: Adapt.px(14),
                                           ),
-                                        )
-                                      : Container(
-                                          child: Row(
-                                            children: [
-                                              Icon(Icons.home),
-                                              Text('成绩不太好，要继续努力鸭')
-                                            ],
-                                          ),
-                                        ),
+                                          Text(
+                                            item.correctQuestion ==
+                                                    item.totalQuestion
+                                                ? '可真厉害，再接再厉吧^_^'
+                                                : '成绩不太好，要继续努力鸭',
+                                            style: TextStyle(
+                                              // height: 2,
+                                              fontSize: Adapt.px(12),
+                                              color: Color(0xFF999999),
+                                            ),
+                                          )
+                                        ]),
+                                  ),
                                 ],
                               )
-                            : Text('暂未练习'),
+                            : Text(
+                                '暂未练习',
+                                style: TextStyle(
+                                  fontSize: Adapt.px(12),
+                                  color: Color(0xFF999999),
+                                ),
+                              ),
                       ],
+                    ),
+                    Positioned(
+                      top: Adapt.px(34),
+                      right: Adapt.px(8),
+                      child: RaisedButton(
+                        padding: EdgeInsets.all(0),
+                        elevation: 0,
+                        onPressed: () {},
+                        color: Color(0xFFCFF3FF),
+                        highlightColor: Color(0xFFCFF3FF),
+                        highlightElevation: 0,
+                        shape: StadiumBorder(),
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: Adapt.px(80),
+                          height: Adapt.px(30),
+                          child: Text(
+                            item.correctQuestion == 0 ? '去练习' : '再练一次',
+                            style: TextStyle(
+                              color: Color(0xFF02AEFB),
+                              fontSize: Adapt.px(14),
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
                   ],
                 ),
               );
             }).toList(),
           );
+  }
+
+  Widget get _wrongQuestionView {
+    return Container(
+      child: ListView(
+        children: [
+          CourseTagList(),
+          Container(
+              margin: EdgeInsets.only(
+                top: Adapt.px(10),
+                left: Adapt.px(26),
+                right: Adapt.px(26),
+              ),
+              padding: EdgeInsets.only(
+                top: Adapt.px(20),
+                left: Adapt.px(24),
+                right: Adapt.px(25),
+                bottom: Adapt.px(14),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(Adapt.px(6))),
+                border: Border.all(width: 1, color: Color(0xFFEBEBEB)),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    "在18+ (18x20-25) ÷25中，最后一步求()",
+                    style: TextStyle(
+                      height: 1.8,
+                      letterSpacing: Adapt.px(1),
+                      fontSize: Adapt.px(16),
+                      color: Color(0xFF333333),
+                    ),
+                  ),
+                  _answerOption('A', '商'),
+                  _answerOption('B', '和'),
+                  _answerOption('C', '积'),
+                ],
+              )),
+        ],
+      ),
+    );
+  }
+
+  Widget _answerOption(String index, String text) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(Adapt.px(6))),
+        border: Border.all(
+          width: 1,
+          color: Color(0xFFB5E8FF),
+        ),
+      ),
+      child: Row(
+        children: [
+          Text(index),
+          Container(
+            child: Text(text),
+          ),
+        ],
+      ),
+    );
   }
 }
